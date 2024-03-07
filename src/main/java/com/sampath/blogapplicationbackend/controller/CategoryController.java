b8a6606db4664967f092590e3ce2e1445f064aed
sampath kumar <jettysampath8@gmail.com>
2024-03-07 23:46:23 +0530

    updated cors

--- src/main/java/com/sampath/blogapplicationbackend/controller/CategoryController.java
+++ src/main/java/com/sampath/blogapplicationbackend/controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@CrossOrigin(value = "http://localhost:3000")
 public class CategoryController {
     @Autowired
     private ICategoryService categoryService;
--- src/main/java/com/sampath/blogapplicationbackend/controller/PostController.java
+++ src/main/java/com/sampath/blogapplicationbackend/controller/PostController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
+@CrossOrigin(value = "http://localhost:3000")
 public class PostController {
 
     @Autowired
